677c86b74928fb8f1c420aada64805ce52797139
teofilushendry5@gmail.com <teofilushendry5@gmail.com>
2019-11-03 10:51:45 +0700

    Scrapping-Youtubev1.2

--- Scrapping-Youtube.py
+++ Scrapping-Youtube.py
@@ -2,13 +2,11 @@ import bs4, requests, os, csv, io
 page = requests.get('https://www.youtube.com/', timeout=(5, None))
 soup = bs4.BeautifulSoup(page.text, 'html.parser')
 
-i = 0
-while os.path.exists('Scrapping-youtube%s.csv' % i):
-        i+=1
-filename = "Scraping-youtube%s.csv" % i
-downloadedFile = csv.writer(io.open(filename,'w', encoding="utf-8"))
+if page.status_code == requests.codes.ok:
+    namaFile = input('Ketik Nama File Yang Diinginkan :\n>> ')
+    openFile = io.open(f'{namaFile}.csv','w', encoding="utf-8")
+    downloadedFile = csv.writer(openFile)
 
-if page.status_code==200:
     print("=" * 30 + "Finished" +"=" * 30)
     title = soup.select('h3 a')
     single = soup.find_all('span', class_="accessible-description")
@@ -22,7 +20,6 @@ if page.status_code==200:
             titlee = a['title']
             course=[titlee,linkVideo]
             titleList.append(course)
-
     def singles():
         for b in single:    
             durasi = b.get_text()
@@ -32,7 +29,6 @@ if page.status_code==200:
         for c in double:
             upload = c.get_text()
             link = "https://www.youtube.com"+c['href']
-
             course=[upload, link]
             titleList.append(course)
     def triples():
@@ -42,6 +38,7 @@ if page.status_code==200:
                 items1 = li.text 
             course=[items2, items1]
             titleList.append(course)
+                
     titles()
     singles()
     doubles()
